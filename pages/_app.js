48f00fc1aaad73825227744579b295fbf76e983d
Ricardo Gomes <sleiph@gmail.com>
2021-01-25 20:58:50 -0300

    fim da primeira aula

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,36 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json'
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Cormorant:wght@300;400;700&display=swap');
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Cormorant', serif;
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
